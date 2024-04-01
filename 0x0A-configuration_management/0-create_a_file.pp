71d4be6b0bdd0f931e1cabbbac2314db35af53e6
adnanenet <bensouda.adnane.dev@gmail.com>
2024-04-01 06:48:30 -0700

    task 0 create file done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creates a file in /tmp
 
 file { '/tmp/school':
-  content =>'I love rubby Puppet',
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
